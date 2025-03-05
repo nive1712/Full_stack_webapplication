951be35d386e269637309399b3bc7ae1d416e28a
S.NIVEDHA <89909984+nive1712@users.noreply.github.com>
2025-03-05 12:48:20 +0530

    Create CardUnblockRequestDto.java

---
+++ springboot/src/main/java/com/bank/dto/CardUnblockRequestDto.java
@@ -0,0 +1,34 @@
+package com.bank.dto;
+
+public class CardUnblockRequestDto {
+
+    private int userId;
+    private String accountNumber;
+    private int pin;
+
+    
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+}
