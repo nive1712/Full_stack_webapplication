1baf28e98201a01c76ce32ec8a2306ecafba9753
S.NIVEDHA <89909984+nive1712@users.noreply.github.com>
2025-04-15 16:15:49 +0530

    Create card-block.component.spec.ts

---
+++ Frontend_Angular/src/app/netbanking/card-block/card-block.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardBlockComponent } from './card-block.component';
+
+describe('CardBlockComponent', () => {
+  let component: CardBlockComponent;
+  let fixture: ComponentFixture<CardBlockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardBlockComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CardBlockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
+
