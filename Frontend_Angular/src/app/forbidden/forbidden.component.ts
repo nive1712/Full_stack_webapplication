47e190d61b03fd4410cb41fb973e5df89d52f900
S.NIVEDHA <89909984+nive1712@users.noreply.github.com>
2025-04-15 15:55:29 +0530

    Create forbidden.component.ts

---
+++ Frontend_Angular/src/app/forbidden/forbidden.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forbidden',
+  template: '<h1>403 - Forbidden</h1><p>You do not have permission to view this page.</p>',
+})
+export class ForbiddenComponent {}
+
